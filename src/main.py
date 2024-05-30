e4bc1502d3972503a44df0da5f35f799b0fbb7ca
bmorris213 <thisisnotalreadyused@gmail.com>
2024-05-29 20:24:59 -0600

    initialize project environment

---
+++ src/main.py
@@ -0,0 +1,14 @@
+# Maze Solver
+# 05-27-24
+# Brian Morris
+
+# Main
+# program begins execution here
+# execution is passed
+def main():
+    # do stuff
+
+    print("not implemented")
+
+if __name__ == '__main__':
+    main()
